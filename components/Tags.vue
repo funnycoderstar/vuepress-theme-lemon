535987907a9ae8e75d77d0e64cf0191489159fd0
funnycoderstar <funnycoderstar@gmail.com>
2019-09-22 23:26:25 +0800

    feat: 添加tags页面

---
+++ components/Tags.vue
@@ -0,0 +1,88 @@
+<template>
+    <div class="tags">
+        <header class="tags-header">
+            <h1>标签</h1>
+        </header>
+        <div class="tag-cloud">
+            <div class="tag-cloud-title">
+                目前共计 {{tagList.length}} 个标签
+            </div>
+        </div>
+        <div class="tag-cloud-tags">
+            <a :href="tag.link" v-for="(tag, index) in tagList" :key="index" class="tag-item">{{tag.tagName}}({{tag.count}})</a>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: '',
+    data() {
+        return {
+            tagList: [
+                {
+                    tagName: 'HTTP',
+                    count: 11,
+                    link: '/tags/HTTP/'
+                },
+                {
+                    tagName: 'HTML',
+                    count: 10,
+                    link: '/tags/HTML/'
+                },
+                {
+                    tagName: 'CSS',
+                    count: 6,
+                    link: '/tags/CSS/'
+                },
+                {
+                    tagName: 'JS',
+                    count: 8,
+                    link: '/tags/JS/'
+                },
+            ]
+        }
+    },
+}
+</script>
+
+<style lang="stylus">
+    .tags {
+        max-width: 740px;
+        margin: 0 auto;
+        padding $navbarHeight 0;
+        position: relative;
+        text-align center;
+        min-height: calc(100vw - 30rem);
+        .tag-cloud-tags {
+            margin-top: 20px;
+            .tag-item {
+                margin-right: 2rem;
+                font-size: 2rem;
+                padding: 1rem 0;
+                font-size: 0.8rem;
+                color: #b1b1b1;
+                border-bottom: 1px solid #b1b1b1;
+                
+            }
+            
+            .tag-item:nth-of-type(2n+1) {
+               color: $textColor;
+                border-bottom: 1px solid $textColor;
+            }
+            .tag-item:nth-of-type(3n+1) {
+                font-size: 1.5rem;
+                color: #666;
+                border-bottom: 1px solid #666;
+            }
+            .tag-item:hover {
+                color: $accentColor;
+                border-bottom: 1px solid $accentColor;
+            }
+        }
+    }
+</style>
+
+
+
+
--- layouts/Layout.vue
+++ layouts/Layout.vue
@@ -1,73 +1,177 @@
 <template>
-  <div class="theme-container">
-    <Navbar @toggle-sidebar="toggleSidebar"/>
-    <Content/>
+  <div
+    class="theme-container"
+    :class="pageClasses"
+    @touchstart="onTouchStart"
+    @touchend="onTouchEnd"
+  >
+    <Navbar
+      v-if="shouldShowNavbar"
+      @toggle-sidebar="toggleSidebar"
+    />
+
+    <div
+      class="sidebar-mask"
+      @click="toggleSidebar(false)"
+    ></div>
+
+    <Sidebar
+      :items="sidebarItems"
+      @toggle-sidebar="toggleSidebar"
+    >
+      <slot
+        name="sidebar-top"
+        slot="top"
+      />
+      <slot
+        name="sidebar-bottom"
+        slot="bottom"
+      />
+    </Sidebar>
+    <Home v-if="$page.frontmatter.home"/>
+    <Post v-if="shouldShowPost"/>
+    <Category v-if="shouldShowCategory"/>
+
+    <Tags v-if="shouldShowTags"/>
+
+    <Page
+      v-else
+      :sidebar-items="sidebarItems"
+    >
+      <slot
+        name="page-top"
+        slot="top"
+      />
+      <slot
+        name="page-bottom"
+        slot="bottom"
+      />
+    </Page>
     <Footer></Footer>
   </div>
 </template>
+
 <script>
-import Navbar from "@theme/components/Navbar.vue";
-import Footer from "@theme/components/Footer.vue";
+import Home from '@theme/components/Home.vue'
+import Navbar from '@theme/components/Navbar.vue'
+import Page from '@theme/components/Page.vue'
+import Sidebar from '@theme/components/Sidebar.vue'
+import Category from '@theme/components/Category.vue'
+import Tags from '@theme/components/Tags.vue'
+import Footer from '@theme/components/Footer.vue'
+import Post from '@theme/components/Post.vue'
+import { resolveSidebarItems } from '../util'
+
 export default {
-    components: {
-        Navbar,
-        Footer,
+  components: { Home, Page, Sidebar, Navbar, Category, Tags, Footer, Post},
+
+  data () {
+    return {
+      isSidebarOpen: false
+    }
+  },
+
+  computed: {
+    shouldShowNavbar () {
+      const { themeConfig } = this.$site
+      const { frontmatter } = this.$page
+      if (
+        frontmatter.navbar === false
+        || themeConfig.navbar === false) {
+        return false
+      }
+      return (
+        this.$title
+        || themeConfig.logo
+        || themeConfig.repo
+        || themeConfig.nav
+        || this.$themeLocaleConfig.nav
+      )
     },
-    data () {
-        return {
-            isSidebarOpen: false,
-            shouldShowNavBar: true,
+
+    shouldShowSidebar () {
+      const { frontmatter } = this.$page
+      return (
+        !frontmatter.home
+        && frontmatter.sidebar !== false
+        && this.sidebarItems.length
+      )
+    },
+
+    sidebarItems () {
+      return resolveSidebarItems(
+        this.$page,
+        this.$page.regularPath,
+        this.$site,
+        this.$localePath
+      )
+    },
+
+    pageClasses () {
+      const userPageClass = this.$page.frontmatter.pageClass
+      return [
+        {
+          'no-navbar': !this.shouldShowNavbar,
+          'sidebar-open': this.isSidebarOpen,
+          'no-sidebar': !this.shouldShowSidebar
+        },
+        userPageClass
+      ]
+    },
+    shouldShowCategory() {
+      const { frontmatter } = this.$page
+      return (
+        frontmatter.type
+        && frontmatter.type === 'category'
+      )
+    },
+    shouldShowTags() {
+      const { frontmatter } = this.$page
+      return (
+        frontmatter.type
+        && frontmatter.type === 'tags'
+      )
+    },
+    shouldShowPost() {
+      const { frontmatter } = this.$page
+      return (
+        frontmatter.title
+        && frontmatter.title === 'Post'
+      )
+    }
+  },
+
+  mounted () {
+    this.$router.afterEach(() => {
+      this.isSidebarOpen = false
+    })
+  },
+
+  methods: {
+    toggleSidebar (to) {
+      this.isSidebarOpen = typeof to === 'boolean' ? to : !this.isSidebarOpen
+      this.$emit('toggle-sidebar', this.isSidebarOpen)
+    },
+
+    // side swipe
+    onTouchStart (e) {
+      this.touchStart = {
+        x: e.changedTouches[0].clientX,
+        y: e.changedTouches[0].clientY
+      }
+    },
+
+    onTouchEnd (e) {
+      const dx = e.changedTouches[0].clientX - this.touchStart.x
+      const dy = e.changedTouches[0].clientY - this.touchStart.y
+      if (Math.abs(dx) > Math.abs(dy) && Math.abs(dx) > 40) {
+        if (dx > 0 && this.touchStart.x <= 80) {
+          this.toggleSidebar(true)
+        } else {
+          this.toggleSidebar(false)
         }
-    },
-    // computed: {
-    //     shouldShowNavBar() {
-    //         const { themeConfig } = this.$site;
-    //         const { frontmatter } = this.$page;
-    //         if (frontmatter.navbar === false || themeConfig.navbar === false) {
-    //             return false;
-    //         }
-    //         return (
-    //             this.$title ||
-    //             themeConfig.logo ||
-    //             themeConfig.repo ||
-    //             themeConfig.nav ||
-    //             this.$themeLocaleConfig.nav
-    //         );
-    //     },
-    // },
-    mounted() {
-         console.log(555, this.$site);
-        this.$router.afterEach(() => {
-            this.isSidebarOpen = false;
-        });
-    },
-    methods: {
-        toggleSidebar(to) {
-            this.isSidebarOpen =
-                typeof to === "boolean" ? to : !this.isSidebarOpen;
-            this.$emit("toggle-sidebar", this.isSidebarOpen);
-        },
-
-        // side swipe
-        onTouchStart(e) {
-            this.touchStart = {
-                x: e.changedTouches[0].clientX,
-                y: e.changedTouches[0].clientY,
-            };
-        },
-
-        onTouchEnd(e) {
-            const dx = e.changedTouches[0].clientX - this.touchStart.x;
-            const dy = e.changedTouches[0].clientY - this.touchStart.y;
-            if (Math.abs(dx) > Math.abs(dy) && Math.abs(dx) > 40) {
-                if (dx > 0 && this.touchStart.x <= 80) {
-                    this.toggleSidebar(true);
-                } else {
-                    this.toggleSidebar(false);
-                }
-            }
-        },
-    },
-};
+      }
+    }
+  }
+}
 </script>
-
