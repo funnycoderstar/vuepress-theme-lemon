a2a9c2a35dc96ea79351d49351e60057413a93ea
funnycoderstar <funnycoderstar@gmail.com>
2019-09-27 00:19:15 +0800

    feat: 添加评论功能需要的一些package

--- index.js
+++ index.js
@@ -54,7 +54,17 @@ module.exports = (themeConfig, ctx) =>  {
         [
             '@vuepress/blog',
             blogPluginOptions,
-          ],
+        ],
+        '@vssue/vuepress-plugin-vssue': {
+            // 设置 `platform` 而不是 `api`
+            platform: 'github',
+      
+            // 其他的 Vssue 配置
+            owner: 'OWNER_OF_REPO',
+            repo: 'NAME_OF_REPO',
+            clientId: 'YOUR_CLIENT_ID',
+            clientSecret: 'YOUR_CLIENT_SECRET',
+          },
 
     ]
     const config = {
